760dac8bb26511731695cbfca37ee93bf2983114
Aniket-bit7 <aniketpathak1073@gmail.com>
2025-05-06 10:57:11 +0530

    adding companies carousel

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
+import CompanyCarousel from "@/components/company-carousel";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { BarChart, Calendar, ChevronRight, Layout } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
 
@@ -72,6 +72,15 @@ export default function Home() {
               </Card>
             ))}
           </div>
+        </div>
+      </section>
+
+      <section className="py-20">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">
+            Trusted by Industry Leaders
+          </h3>
+          <CompanyCarousel />
         </div>
       </section>
     </div>
---
+++ components/company-carousel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompanyCarousel = () => {
+  return (
+    <div>company-carousel</div>
+  )
+}
+
+export default CompanyCarousel;
